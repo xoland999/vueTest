0e6e1b3a2920b7ce67199ef1700820d00a4a282a
ZEN <xoland999@yahoo.com.tw>
2023-03-15 11:20:47 +0800

    新增router設定

--- my-app/src/router/index.ts
+++ my-app/src/router/index.ts
@@ -9,12 +9,22 @@ const routes: Array<RouteRecordRaw> = [
   },
   {
     path: "/about",
-    name: "about",
+    name: "aboutA",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/books",
+    name: "bookListPage",
+    component: () => import("../views/BookListView.vue"),
+  },
+  {
+    path: "/books/:bookId",
+    name: "bookDetailPage",
+    component: () => import("../views/BookDetailView.vue"),
   },
 ];
 
---
+++ my-app/src/views/BookListView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>This is A view</div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>
+<style scoped></style>
